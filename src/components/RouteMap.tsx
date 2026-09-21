50cb11136f162921655e469475bb0f57bac8b8b5
jemeresh <klavdea.sharipova@gmail.com>
2026-09-21 18:50:12 +0300

    added zoning to the map after zooming and moving

--- src/components/RouteMap.tsx
+++ src/components/RouteMap.tsx
@@ -1,21 +1,33 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, useMapEvents } from "react-leaflet";
 import 'leaflet/dist/leaflet.css'
 import { useState } from "react";
 
+function BboxTracker({onBboxChange}) {
+    const map = useMapEvents(
+    {
+      moveend: () => { onBboxChange(map.getBounds().toBBoxString()) },
+      zoomend: () => { onBboxChange(map.getBounds().toBBoxString()) }
+    })
+    return null
+}
 
 function RouteMap () {
 
-  const mapOptions = {
-        center: [ 17.385044, 78.486671],
-        zoom: 10
-    }
+  const [bbox,setBbox] = useState('')
 
+  console.log(bbox)
+
+  const mapOptions = {
+        center: [ 45.385044, 45.486671],
+        zoom: 15
+    }
 
   return (
     <>
         <MapContainer {...mapOptions} style = {{width: "100%", height: "500px"}}>
+          <BboxTracker onBboxChange={setBbox} />
           <TileLayer url = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'/>
-          </MapContainer>
+        </MapContainer>
     </>
   )
 }
